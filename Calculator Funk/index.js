4360a454607895298c2ebdb93d9f053594d69916
Michael Correa <98114967+scnc-tch@users.noreply.github.com>
2022-03-27 11:49:14 -0400

    done 3.27.22

---
+++ Calculator Funk/index.js
@@ -0,0 +1,15 @@
+
+
+
+function clear(){
+    let result = 0;
+    document.getElementById("input").textContent = result;
+}
+
+function eight(){
+    document.getElementById("input").textContent = i
+}
+
+//localStorage.setItem("dog", "finn")
+let dog = localStorage.getItem("dog");
+console.log(dog)
